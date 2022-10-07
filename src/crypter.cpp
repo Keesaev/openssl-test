677c1171abd48cd2c3759a8f1e530b10da14259a
Keesaev <kostia.234@mail.ru>
2022-10-07 18:54:53 +0300

    encrypting and decrypting byte array

--- src/crypter.cpp
+++ src/crypter.cpp
@@ -55,36 +55,43 @@ std::basic_string<uint8_t> crypter::encrypt(std::basic_string<uint8_t> const& in
         return out_str;
     }
 
-    out_str = std::basic_string<uint8_t>(out.get(), outlen);
-    return out_str;
+    return std::basic_string<uint8_t>(out.get(), outlen);
 }
 
-std::basic_string<uint8_t> crypter::decrypt(std::basic_string<uint8_t> const& in)
+std::basic_string<uint8_t> crypter::decrypt(std::basic_string<uint8_t> const& in_str)
 {
     std::basic_string<uint8_t> out_str;
-    return out_str; // TODO RM
-    /*
-        if (EVP_PKEY_decrypt_init(_ctx) <= 0) {
-            std::cerr << "EVP_PKEY_encrypt failed" << std::endl;
-            return out_str;
-        }
 
-        if (EVP_PKEY_CTX_set_rsa_padding(_ctx, RSA_PKCS1_OAEP_PADDING) <= 0) {
-            std::cerr << "EVP_PKEY_encrypt failed" << std::endl;
-            return out_str;
-        }
-        if (EVP_PKEY_decrypt(_ctx, NULL, &outlen, in, inlen) <= 0) {
-            std::cerr << "EVP_PKEY_encrypt failed" << std::endl;
-            return out_str;
-        }
-        out = (uint8_t*)OPENSSL_malloc(outlen);
+    if (EVP_PKEY_decrypt_init(_ctx) <= 0) {
+        std::cerr << "EVP_PKEY_encrypt failed" << std::endl;
+        return out_str;
+    }
 
-        if (!out) {
-            std::cerr << "EVP_PKEY_encrypt failed" << std::endl;
-            return out_str;
-        }
-        if (EVP_PKEY_decrypt(_ctx, out, &outlen, in, inlen) <= 0) {
-            std::cerr << "EVP_PKEY_encrypt failed" << std::endl;
-            return out_str;
-        }*/
+    if (EVP_PKEY_CTX_set_rsa_padding(_ctx, RSA_PKCS1_OAEP_PADDING) <= 0) {
+        std::cerr << "EVP_PKEY_encrypt failed" << std::endl;
+        return out_str;
+    }
+
+    size_t outlen;
+    if (EVP_PKEY_decrypt(_ctx, NULL, &outlen, in_str.c_str(), in_str.size()) <= 0) {
+        std::cerr << "EVP_PKEY_encrypt failed" << std::endl;
+        return out_str;
+    }
+
+    auto openssl_deleter = [](uint8_t* a) {
+        OPENSSL_free(a);
+    };
+
+    auto out = std::unique_ptr<uint8_t, decltype(openssl_deleter)>((uint8_t*)OPENSSL_malloc(outlen), openssl_deleter);
+
+    if (!out.get()) {
+        std::cerr << "EVP_PKEY_encrypt failed" << std::endl;
+        return out_str;
+    }
+    if (EVP_PKEY_decrypt(_ctx, out.get(), &outlen, in_str.c_str(), in_str.size()) <= 0) {
+        std::cerr << "EVP_PKEY_encrypt failed" << std::endl;
+        return out_str;
+    }
+
+    return std::basic_string<uint8_t>(out.get(), outlen);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,28 @@
 
 #include "crypter.h"
 
-int main(){
+int main()
+{
+    std::basic_string<uint8_t> source { 0x01, 0x02, 0x03 };
+
+    std::cout << "SOURCE (of size = " << source.size() << ")\n";
+    for(const int& i : source){
+        std::cout << i << ' ';
+    }
+    std::cout << std::endl;
 
     crypter c;
-    std::basic_string<uint8_t> in;
-    auto out = c.encrypt(in);
+    auto encrypted = c.encrypt(source);
 
-    for(const int& i : out){
+    std::cout << "ENCRYPTED (of size = " << encrypted.size() << ")\n";
+    for (const int& i : encrypted) {
+        std::cout << i << ' ';
+    }
+
+    auto decrypted = c.decrypt(encrypted);
+
+    std::cout << "\nDECRYPTED (of size = " << decrypted.size() << ")\n";
+    for (const int& i : decrypted) {
         std::cout << i << ' ';
     }
     std::cout << std::endl;
